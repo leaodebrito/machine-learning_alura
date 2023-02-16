56ea94de515b028ddb2b800c3b1b7172fd9546e4
Bruno Leão <leaodebrito@icloud.com>
2023-02-16 16:50:24 -0300

    testagem do código de ML em python

--- 1. Primeiros passos/intro_ML_classificacao_3.py
+++ 1. Primeiros passos/intro_ML_classificacao_3.py
@@ -1,11 +1,34 @@
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 #from sklearn.model_selection import train_test_split
 #from sklearn.svm import LinearSVC
 #from sklearn.metrics import accuracy_score
 
-
-uri = 'https://gist.githubusercontent.com/guilhermesilveira/1b7d5475863c15f484ac495bd70975cf/raw/16aff7a0aee67e7c100a2a48b676a2d2d142f646/projects.csv'
-
+#importação CSV
+uri = '1. Primeiros passos/projects.csv'
 dados = pd.read_csv(uri)
+
+
+#mudança do títulos das colunas
+mapa = {
+    'expected_hours': 'horas_esperadas',
+    'price': 'preco',
+    'unfinished': 'nao_finalizado'
+}
+dados = dados.rename(columns=mapa)
+
+#ajuste de valores para nova coluna 'finalizado'
+troca = {
+    1:0,
+    0:1
+}
+dados['finalizado'] = dados.nao_finalizado.map(troca)
+
+
+#plotagem de gráficos usando seaborn
+#plotar usando mathplotlib
+
+print(sns.scatterplot(x='horas_esperadas', y='preco', hue='finalizado', data=dados))
+sns.relplot(x = "horas_esperadas", y = "preco",hue = "finalizado", col = "finalizado", data=dados)
